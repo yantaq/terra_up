5202e851cdfbfb67cb2ef9896fcbaa4bbb0c1164
Yulghun Yantaq <yantaq@att.net>
2021-04-18 13:00:50 -0700

    Added var and output

--- main.tf
+++ main.tf
@@ -5,8 +5,37 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0bdb828fd58c52235"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup python -m SimpleHTTPServer ${var.server_port} &
+              EOF
   
   tags = {
    Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port server will use for http requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
