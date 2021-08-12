3e47163ad33f44c0f5914bc334ea070ba3259ffd
Yulghun Yantaq <yantaq@att.net>
2021-08-12 00:49:43 -0700

    added asg

---
+++ terraform-up-running/main.tf
@@ -0,0 +1,152 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0bdb828fd58c52235"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup python -m SimpleHTTPServer ${var.server_port} &
+              EOF
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids  
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key = "Name"
+    value = var.project_name
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name = var.project_name
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port = var.alb_to_from_port
+    to_port = var.alb_to_from_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  # Allow all outbound requests
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = var.project_name
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+variable "alb_to_from_port" {
+  type = number
+  default = 80
+}
+variable "server_port" {
+  description = "The port server will use for http requests"
+  type = number
+  default = 8080
+}
+
+variable "project_name" {
+  description = "name of this project - terraform-asg-example"
+  type = string 
+  default = "terraform-asg-example"
+}
+
+output "public_ip" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
